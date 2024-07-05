8207dd944e5c8bda9eeef65ec539de76dd734ebf
mlobeto <mercedeslobeto@gmail.com>
2024-07-05 15:14:25 -0300

    Local

--- Number-boardApi/src/database/database.js
+++ Number-boardApi/src/database/database.js
@@ -5,13 +5,13 @@ import dotenv from 'dotenv';
 // Cargar variables de entorno desde un archivo .env
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: false,  // puedes activar esto para ver las consultas SQL en la consola
-});
-//  export const sequelize = new Sequelize(process.env.DB_DEPLOY, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// }
-// );
+// export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   dialect: 'postgres',
+//   logging: false,  // puedes activar esto para ver las consultas SQL en la consola
+// });
+ export const sequelize = new Sequelize(process.env.DB_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+}
+);
